55a688b811e99ddbc284aa93a81ffd4bf5070560
charles <charleshopkinsiv@gmail.com>
2021-11-23 14:43:31 +0000

    Added analytics

--- views/inc/head.php
+++ views/inc/head.php
@@ -1,4 +1,15 @@
 <head>
+
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-K55L3Y7K1Y"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-K55L3Y7K1Y');
+  </script>
+
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
